ce73cd82481ec3d883a284e31a2589b44fd437a6
AdrianaSaty <adri-saty@hotmail.com>
2023-08-04 16:15:48 -0300

    fix: header

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 const input = document.querySelector('#city');
 const temperatureDiv = document.querySelector('#temperature');
 
-function searchWeather () {
-    
+function searchWeather() {
+
     const city = input.value;
     const lang = 'pt';
     const celsius = 'metric'
-    const API = '3ed581ededecc702e7c6bf69d2351db9';
+    const APIkey = '3ed581ededecc702e7c6bf69d2351db9';
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=${lang}&units=${celsius}&appid=${API}`;
-    
-    fetch(url)
-    .then (response => response.json())
-    .then (data => {
-        const temp = Math.round(data.main.temp);
-        const description = data.weather[0].description;
-        const icon = data.weather[0].icon;
-        const iconUrl = `https://openweathermap.org/img/w/${icon}.png`;
 
-        temperatureDiv.innerHTML = `
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const temp = Math.round(data.main.temp);
+            const description = data.weather[0].description;
+            const icon = data.weather[0].icon;
+            const iconUrl = `https://openweathermap.org/img/w/${icon}.png`;
+
+            temperatureDiv.innerHTML = `
         <h2>${city}</h2>
         <img src="${iconUrl}" alt="${description}">
         <p class="capitalize">${description}</p>
         <p>Temperatura: ${temp}°C</p>`
-    })
-    .catch(error => console.error(error));
+        })
+        .catch(error => console.error(error));
 }
